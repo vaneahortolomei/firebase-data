c30ba86f97af2cf58c70026ef05bb6dd0bae63d1
Ваня Хортоломей <hortolomei1989@gmail.com>
2023-04-14 14:32:21 +0300

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div>{{ string }} Vanea!</div>
+  <img src="/vite.svg" alt="image"/>
+  <img src="/vite.svg" alt="image"/>
+  <img src="/vite.svg" alt="image"/>
 </template>
 <script setup>
     import {ref} from "vue";
 
-    console.log("fefe");
-
     const string = ref("hello");
+    const number = ref(1);
+
+    console.log("number", number.value);
 </script>
