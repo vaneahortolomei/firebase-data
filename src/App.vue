c858fe2e4b9dc7d9ef10fabcff8ec6b1fc9dfe32
Ваня Хортоломей <hortolomei1989@gmail.com>
2023-04-14 13:23:29 +0300

    plus config eslint

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>{{string}} Vanea!</div>
+  <div>{{ string }} Vanea!</div>
 </template>
 <script setup>
-    import {ref} from 'vue';
+    import {ref} from "vue";
 
-    console.log('fefe');
+    console.log("fefe");
 
-    const string = ref('hello');
+    const string = ref("hello");
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import {createApp} from 'vue'
-import './style/style.scss'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style/style.scss";
+import App from "./App.vue";
 
 const app = createApp(App);
 
-app.mount('#app');
+app.mount("#app");
