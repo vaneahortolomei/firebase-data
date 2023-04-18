32126b184238982873716a932687a7fdfd67d9e1
Ваня Хортоломей <hortolomei1989@gmail.com>
2023-04-18 13:22:21 +0300

    add router,new pages,components

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <nav>
+      <ul class="list">
+        <li class="list__item">
+          <router-link
+            :to="{name: 'home'}"
+            class="list__link"
+          >
+            Home
+          </router-link>
+        </li>
+        <li class="list__item">
+          <router-link
+            :to="{name: 'dashboard'}"
+            class="list__link"
+          >
+            Dashboard
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/pages/DashboardPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    Dashboard
+    <List />
+
+    <ul>
+      <li>{{ response }}</li>
+      <li>{{ data }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+    import List from "../components/ListItems.vue";
+
+    import axios from "axios";
+    import {ref} from "vue";
+
+    const response = ref("");
+    const data = ref("");
+
+    axios.get("//localhost:3000/dashboard")
+        .then(data => {
+            return data.value = data.data;
+        })
+        .catch(e => {
+            return response.value = e.message;
+        })
+        .finally(() => "data is fetched!");
+</script>
